4cfa01c7982b3c28ae5619d4eca9437ddcb1b6d6
gekaiyuan <gekaiyuan118@163.com>
2024-01-03 16:28:12 +0800

    test

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -27,8 +27,8 @@ class ProductController extends AdminController
 
         $product = Product::where('offer_status',1)->get()->toArray();
 
-        print_r("<pre/>");
-        print_r($product);exit;
+//        print_r("<pre/>");
+//        print_r($product);exit;
 
         return $content->title('详情')
             ->description('简介')
