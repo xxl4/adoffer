7fc82bfeeb7c72d709d939f3344f4cf857ce229a
gekaiyuan <gekaiyuan118@163.com>
2024-02-26 14:59:51 +0800

    修改名称

--- app/Admin/Controllers/OfferTrackController.php
+++ app/Admin/Controllers/OfferTrackController.php
@@ -80,7 +80,7 @@ class OfferTrackController extends AdminController
         $form->text('track_name', __('Track name'))->required();
 //        $form->url('track_link', __('Track Link'))->required();
         $form->select('land_id', __('Land Page'))->options(LandPage::all()->pluck('land_link', 'id'))->required();
-        $form->select('track_type_id', __('Track Type'))->options(OfferTracksCates::all()->pluck('track_cate', 'id'))->required();
+        $form->select('track_type_id', __('Track Tab'))->options(OfferTracksCates::all()->pluck('track_cate', 'id'))->required();
         $form->datetime('created_at', __('Create at'))->default(date('Y-m-d H:i:s'));
 //        $form->datetime('update_at', __('Update at'))->default(date('Y-m-d H:i:s'));
 
