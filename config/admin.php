c2f940b496a91998f5ba19dceb326bc7189d085e
gekaiyuan <gekaiyuan118@163.com>
2024-03-26 09:29:45 +0800

    菜单默认展开

--- config/admin.php
+++ config/admin.php
@@ -292,7 +292,7 @@ return [
     | "sidebar-mini".
     |
     */
-    'layout' => ['sidebar-mini', 'sidebar-collapse'],
+    'layout' => ['sidebar-mini'],
 
     /*
     |--------------------------------------------------------------------------
